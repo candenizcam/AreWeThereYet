6205e83f0f572f22c76869aff39b8b3b353f9476
e2036192 <e2036192@ceng.metu.edu.tr>
2021-11-05 20:20:36 +0300

    Runs on native now

--- src/commonMain/kotlin/application/GlobalAccess.kt
+++ src/commonMain/kotlin/application/GlobalAccess.kt
@@ -1,11 +1,14 @@
 package application
 
 import com.soywiz.korma.geom.SizeInt
+import kotlin.native.concurrent.ThreadLocal
 
 /** This file contains variables for global access in the application,
  * user can manipulate here during development
  */
+@ThreadLocal
 object GlobalAccess {
     val virtualSize = SizeInt(1280, 720) // Virtual Size, this is the size that code pretends is the size of the screen
+
     var windowSize = SizeInt(1280, 720) // Window Size, this is the actual size of the window in pixels
 }
