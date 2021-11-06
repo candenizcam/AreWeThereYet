93f7299679579c3b4e58108900b0694d706beb4d
candenizcam <e193777@metu.edu.tr>
2021-11-06 16:14:28 +0300

    obstacle hitboxed

--- src/commonMain/kotlin/application/Obstacle.kt
+++ src/commonMain/kotlin/application/Obstacle.kt
@@ -11,10 +11,61 @@ data class Obstacle(var type: ObstacleTypes, var centerX: Double, var centerY: D
     }
 }
 enum class ObstacleTypes{
-    LOWJUMP,
-    HIGHJUMP,
-    DUCK,
-    LONGJUMP,
-    DONTJUMP,
-    JUMPDUCT
+    LOWJUMP {
+        override fun relevantID(rarity: Int): String {
+            return "low-jump-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(476.0/1980.0,600.0,200.0,0.0)
+        }
+    },
+    HIGHJUMP {
+        override fun relevantID(rarity: Int): String {
+            return "high-jump-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(88.0/1920.0,336.0/1920.0,0.0/1080.0+240.0/1080.0,340.0/1080.0+240.0/1080.0)
+        }
+    },
+    DUCK {
+        override fun relevantID(rarity: Int): String {
+            return "duck-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(778.0/1920.0,902.0/1920.0,140.0/1080.0+240.0/1080.0,840.0/1080.0+240.0/1080.0)
+        }
+    },
+    LONGJUMP {
+        override fun relevantID(rarity: Int): String {
+            return "long-jump-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(1042.0/1920.0,1290.0/1920.0,0.0/1080.0+240.0/1080.0,200.0/1080.0+240.0/1080.0)
+        }
+    },
+    DONTJUMP {
+        override fun relevantID(rarity: Int): String {
+            return "dont-jump-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(1373.0/1920.0,1713.0/1920.0,500.0/1080.0+240.0/1080.0,840.0/1080.0+240.0/1080.0)
+        }
+    },
+    JUMPDUCT {
+        override fun relevantID(rarity: Int): String {
+            return "jump-duck-1"
+        }
+
+        override fun ratedRect(): Rectangle {
+            return Rectangle(1621.0/1920.0,1761.0/1920.0,140.0/1080.0+240.0/1080.0,340.0/1080.0+240.0/1080.0)
+        }
+    };
+
+    abstract fun relevantID(rarity:Int = 0): String
+    abstract fun ratedRect(): Rectangle
 }
