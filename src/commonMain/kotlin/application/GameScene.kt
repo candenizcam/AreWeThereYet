a3fd8a144afd32f2257a5b3e282db3e295b842db
candenizcam <e193777@metu.edu.tr>
2021-11-07 11:19:56 +0300

    pencere açılınca oyun başlıyor

--- src/commonMain/kotlin/application/GameScene.kt
+++ src/commonMain/kotlin/application/GameScene.kt
@@ -4,6 +4,7 @@ import com.soywiz.korau.sound.PlaybackParameters
 import com.soywiz.korau.sound.PlaybackTimes
 import com.soywiz.korau.sound.readMusic
 import com.soywiz.korev.Key
+import com.soywiz.korge.input.onClick
 import com.soywiz.korge.internal.KorgeInternal
 import com.soywiz.korge.view.Container
 import com.soywiz.korge.view.Image
@@ -169,121 +170,115 @@ class GameScene : PunScene() {
 
 
         this.addUpdater { dt ->
-
-
-            /*
-            obstacles.forEach {
-                it.x = it.x - dt.milliseconds*0.2
-            }
-
-             */
-
-            obstacles.forEach {
-                it.visible = false
-            }
+            if(doUpdate){
+                obstacles.forEach {
+                    it.visible = false
+                }
 
 
 
-            outside1.x -= dt.seconds * playfield.level.speed * 1920
-            outside2.x -= dt.seconds * playfield.level.speed * 1920
-            if (outside1.x + outside1.width < -1000.0) {
-                outside1.x += outside1.width * 2
-            }
-            if (outside2.x + outside2.width < -1000.0) {
-                outside2.x += outside2.width * 2
-            }
+                outside1.x -= dt.seconds * playfield.level.speed * 1920
+                outside2.x -= dt.seconds * playfield.level.speed * 1920
+                if (outside1.x + outside1.width < -1000.0) {
+                    outside1.x += outside1.width * 2
+                }
+                if (outside2.x + outside2.width < -1000.0) {
+                    outside2.x += outside2.width * 2
+                }
 
 
-            val obshit = children.filterIsInstance<Puntainer>().filter { it.id == "obshit" }
-            var obshitindex = 0
-            obshit.forEach {
-                it.visible = false
-            }
+                val obshit = children.filterIsInstance<Puntainer>().filter { it.id == "obshit" }
+                var obshitindex = 0
+                obshit.forEach {
+                    it.visible = false
+                }
 
 
 
-            val goreText = if(GlobalAccess.fingers==2){
-                ""
-            }else{
-                "-gore"
-            }
-            ObstacleTypes.values().forEach { thisType ->
-                playfield.level.obstacles.filter { it.type == thisType }.also { list ->
-                    ObstacleRarity.values().forEach { rarity ->
-                        val obstacleData = list.filter { it.obstacleRarity ==rarity }
-                        val o2 = obstacles.filter { it.id == thisType.relevantID(rarity.ordinal+1)+goreText }
+                val goreText = if(GlobalAccess.fingers==2){
+                    ""
+                }else{
+                    "-gore"
+                }
+                ObstacleTypes.values().forEach { thisType ->
+                    playfield.level.obstacles.filter { it.type == thisType }.also { list ->
+                        ObstacleRarity.values().forEach { rarity ->
+                            val obstacleData = list.filter { it.obstacleRarity ==rarity }
+                            val o2 = obstacles.filter { it.id == thisType.relevantID(rarity.ordinal+1)+goreText }
 
-                        obstacleData.forEachIndexed { index, obs ->
-                            val hit = playfield.virtualRectangle.fromRated(
-                                Rectangle(
-                                    Vector(obs.centerX, obs.centerY),
-                                    obs.width,
-                                    obs.height
+                            obstacleData.forEachIndexed { index, obs ->
+                                val hit = playfield.virtualRectangle.fromRated(
+                                    Rectangle(
+                                        Vector(obs.centerX, obs.centerY),
+                                        obs.width,
+                                        obs.height
+                                    )
                                 )
-                            )
-                            val r = hit.decodeRated(thisType.ratedRect())
-                            o2[index].x = r.left
-                            o2[index].yConv = r.top
-                            o2[index].scaledHeight = r.height
-                            o2[index].scaledWidth = r.width
-                            o2[index].visible = true
-                            obshit[obshitindex].scaledWidth = hit.width
-                            obshit[obshitindex].scaledHeight = hit.height
-                            obshit[obshitindex].x = hit.left
-                            obshit[obshitindex].y = GlobalAccess.virtualSize.height - hit.top
-                            obshit[obshitindex].visible = true
-                            obshitindex += 1
+                                val r = hit.decodeRated(thisType.ratedRect())
+                                o2[index].x = r.left
+                                o2[index].yConv = r.top
+                                o2[index].scaledHeight = r.height
+                                o2[index].scaledWidth = r.width
+                                o2[index].visible = true
+                                obshit[obshitindex].scaledWidth = hit.width
+                                obshit[obshitindex].scaledHeight = hit.height
+                                obshit[obshitindex].x = hit.left
+                                obshit[obshitindex].y = GlobalAccess.virtualSize.height - hit.top
+                                obshit[obshitindex].visible = true
+                                obshitindex += 1
+                            }
                         }
-                   }
+                    }
+                }
+
+                if (views.input.keys.justPressed(Key.UP)) {
+                    playfield.jump()
+                }
+
+                if (views.input.keys.justPressed(Key.DOWN)) {
+                    playfield.duck()
+                }
+
+                if (views.input.keys.pressing(Key.DOWN).not()) {
+                    playfield.stopDuck()
+                }
+
+                val r = playfield.virtualRectangle.fromRated(playfield.hitboxRect)
+
+                if(playfield.collisionCheck()){
+                    hand.cutFinger()
+                    GlobalAccess.fingers= 1
+                }
+                else if(playfield.ducking>0.0){
+                    hand.onDuck()
+                } else if (playfield.jumping) {
+                    hand.onAir()
+                } else {
+                    hand.onGround()
+
+                }
+
+                hand.update(dt, r)
+                playfield.update(dt)
+
+
+                floor.visible = hand.activeAnimationType==Hand.ActiveAnimationType.TWOFINGER_CUT
+                floor.alpha = -1*hand.animIndex*hand.animIndex*8.0/605.0 + hand.animIndex*8.0/55.0
+
+                if (playfield.collisionCheck()) {
+                    fadein = true
+                }
+
+                if (fadein) {
+                    if (l2.volume < 0.6) l2.volume += 0.1
+                    else fadein = false
                 }
             }
-
-            if (views.input.keys.justPressed(Key.UP)) {
-                playfield.jump()
-            }
-
-            if (views.input.keys.justPressed(Key.DOWN)) {
-                playfield.duck()
-            }
-
-            if (views.input.keys.pressing(Key.DOWN).not()) {
-                playfield.stopDuck()
-            }
-
-            val r = playfield.virtualRectangle.fromRated(playfield.hitboxRect)
-
-            if(playfield.collisionCheck()){
-                hand.cutFinger()
-                GlobalAccess.fingers= 1
-            }
-            else if(playfield.ducking>0.0){
-                hand.onDuck()
-            } else if (playfield.jumping) {
-                hand.onAir()
-            } else {
-                hand.onGround()
-
-            }
-
-            hand.update(dt, r)
-            playfield.update(dt)
-
-
-            floor.visible = hand.activeAnimationType==Hand.ActiveAnimationType.TWOFINGER_CUT
-            floor.alpha = -1*hand.animIndex*hand.animIndex*8.0/605.0 + hand.animIndex*8.0/55.0
-
-            if (playfield.collisionCheck()) {
-                fadein = true
-            }
-
-            if (fadein) {
-                if (l2.volume < 0.6) l2.volume += 0.1
-                else fadein = false
-            }
-
         }
         super.sceneAfterInit()
     }
+
+    var doUpdate = false
 
     val hand = Hand("hand", oneRectangle())
     var playfield = Playfield("playfield", oneRectangle())
