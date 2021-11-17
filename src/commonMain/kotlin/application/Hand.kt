0ad1cc8a769fdc38ae30b5111586259b19fd38c6
candenizcam <e193777@metu.edu.tr>
2021-11-17 23:48:21 +0300

    commit problem fixed

--- src/commonMain/kotlin/application/Hand.kt
+++ src/commonMain/kotlin/application/Hand.kt
@@ -4,17 +4,44 @@ import com.soywiz.klock.TimeSpan
 import com.soywiz.korge.internal.KorgeInternal
 import com.soywiz.korge.view.Image
 import com.soywiz.korge.view.View
-import com.soywiz.korge.view.position
-import com.soywiz.korge.view.solidRect
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
 import kotlinx.coroutines.DelicateCoroutinesApi
-import modules.basic.Colour
 import pungine.Puntainer
 import pungine.geometry2D.Rectangle
+
 
 @DelicateCoroutinesApi
 @KorgeInternal
 class Hand(id: String? = null, relativeRectangle: Rectangle) : Puntainer(id, relativeRectangle) {
 
+    var fingerCut: Puntainer
+
+
+    suspend fun suspendInitAlternative(fingerNo: String){
+
+        ActiveAnimationType.values().forEach { it ->
+            val punt= Puntainer("${fingerNo}_${it.animationID()}")
+            this.addChild(punt)
+            if(fingerNo=="two"){
+                it.sourceList()
+            }else{
+                it.sourceListOne()
+            }.forEach {s->
+                Image(resourcesVfs[s].readBitmap()).also { it2->
+                    it2.visible = false
+                    punt.addChild(it2)
+                }
+            }
+
+
+        }
+    }
+
+
+
+
+    private val animationSpeed = 15 //12 frames per second
+
+    
 }
